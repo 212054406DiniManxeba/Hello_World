5678646a102f0f1447e71dda9904dd7b97376948
Dini <diniman14@gmail.com>
2020-01-09 19:25:56 +0200

    [DiniM] - create: readOutDuplicates method

--- Hello_World.java
+++ Hello_World.java
@@ -3,10 +3,26 @@
  *
  */
 
+
 public class Hello_World
 {		
 		public static void main(String[] args)
 		{
 			System.out.println("Hello World");
+		}
+		
+		//This method takes in a string array and prints out the duplicates
+		public void readOutDuplicates(String[] names)
+		{
+			for(int x = 0; names.length; x++)
+			{
+				for(int y = x+1; names.length; y++)
+				{
+					if(names[x].equalsIgnoreCase(names[y]))
+					{
+						System.out.println(names[x]+" ");
+					}
+				}
+			}
 		}
 }
